752cf232bf514c5be39daa9da3f53d6dab5f3471
ptlillis <73671618+ptlillis@users.noreply.github.com>
2021-03-06 22:53:49 -0500

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,21 @@
+//requiring orm file from config folder
 var orm = require('../config/orm');
 
+//defining burger model based on ORM functions pulled in above
 const burger = {
+    //select all burgers, using ORM
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
+    //insert one burger to the table
     insertOne: function(column, value, cb) {
         orm.insertOne("burgers", column, value, function(res) {
             cb(res);
         });
     },
+    //update existing burger
     updateOne: function (burgerId, cb) {
         orm.updateOne("burgers", "devoured", burgerId, function (res) {
           cb(res);
