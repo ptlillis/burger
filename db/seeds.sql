0bd457c8b4f41328d44d62df47b10c185e01f35a
ptlillis <ptlillisdc@gmail.com>
2021-03-06 22:41:21 -0500

    pushing code after rigorous testing, sill some issues on display but getting full functionality

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hawaiian Burger", false);
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require('../config/orm');
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(column, value, cb) {
+        orm.insertOne("burgers", column, value, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function (burgerId, cb) {
+        orm.updateOne("burgers", "devoured", burgerId, function (res) {
+          cb(res);
+        });
+      }
+  
+
+};
+
+module.exports = burger;
