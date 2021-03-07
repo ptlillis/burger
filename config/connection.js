7511f5c70c3336fe6f4a1dd21d5ed8750974ed0f
ptlillis <ptlillisdc@gmail.com>
2021-03-06 23:32:06 -0500

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
+var connection
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  const connection = mysql.createConnection({
+    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'wmfgwt0qm9h7toq2',
+    password: 'mci3bt2evarulbqm',
+    database: 'uyqp61j6t4fhsihi'
+  });
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'password',
-  database: 'burger_db',
-});
+};
 
 connection.connect((err) => {
   if (err) {
